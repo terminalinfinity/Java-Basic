0a63b060614c4076b2df06ac5c98eb830961f696
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-10-14 13:51:27 +0600

    Add files via upload

---
+++ math/Boolean.java
@@ -0,0 +1,14 @@
+
+package pkgboolean;
+
+public class Boolean {
+    public static void main(String[] args) {
+       int x=20;
+       int y=40;
+       System.out.println(x>y);
+       System.out.println(x<y);
+       System.out.println(x==y);
+       System.out.println(x!=y);
+       System.out.println(x>=10);
+    }
+}
