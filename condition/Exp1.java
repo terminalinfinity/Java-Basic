1f9846e471a036cdda8d622bfcc5732218311557
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-10-14 20:45:57 +0600

    Add files via upload

---
+++ condition/Exp1.java
@@ -0,0 +1,16 @@
+
+package condition;
+
+public class Exp1 {
+
+    public static void main(String[] args) {
+      int x=10;
+      int y=30;
+      if(x>y){
+          System.out.println("x is greater than y");
+      }
+      else{
+          System.out.println("y is greater than x");
+      }
+    }
+}
---
+++ condition/Exp2.java
@@ -0,0 +1,15 @@
+
+package condition;
+
+public class Exp2 {
+    public static void main(String[] args){
+        int x=50;
+        if(x>0){
+            System.out.println("x is positive");
+        }
+        
+        else{
+            System.out.println("x is negative");
+        }
+    }
+}
