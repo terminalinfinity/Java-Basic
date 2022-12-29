1a737c2a59d9ef8ccac76fbbeb5e7dfbf6e222cb
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-12-29 06:28:52 -0800

    Add files via upload

---
+++ operator/Ternary.java
@@ -0,0 +1,10 @@
+package ControlStatements;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int num=15;
+        //Using ternary operator
+        String output=(num%2==0)?"Even number":"Odd Number";
+        System.out.println(output);
+    }
+}
