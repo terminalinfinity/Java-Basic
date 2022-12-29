6da0bbee4182b52b67500dcb1d9e16bc67cfbfab
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-12-29 06:27:30 -0800

    Add files via upload

---
+++ operator/Decrement.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Decrement {
+    public static void main(String[] args) {
+        int i=5;
+        System.out.println("Before PostDecrement:"+i);
+        System.out.println("    PostDecrement:"+i--);
+        System.out.println("After PostDecrement:"+i);
+        int j=10;
+        System.out.println("Before PreDecrement:"+j);
+        System.out.println("     PostDecrement:"+ --j);
+        System.out.println("Before PostDecrement:"+j);
+    }
+}
---
+++ operator/Increment.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Increment {
+    public static void main(String[] args) {
+        int i=5;
+        System.out.println("Before PostIncrement:"+i);
+        System.out.println("    PostIncrement:"+i++);
+        System.out.println("After PostIncrement:"+i);
+        int j=10;
+        System.out.println("Before PreIncrement:"+j);
+        System.out.println("     PostIncrement:"+ ++j);
+        System.out.println("Before PostIncrement:"+j);
+    }
+}
