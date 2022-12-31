d9885da417b486365235719e4430a8a1c976c683
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-12-31 04:03:13 -0800

    Add files via upload

---
+++ operator/Logical.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Logical {
+    public static void main(String[] args) {
+        int a=2,b=5,c=1;
+        System.out.println(a<b && c>a);//True & False=False
+        System.out.println(a<b || c>a);//True & False=True
+    }
+}
---
+++ operator/Shift.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Shift {
+    public static void main(String[] args) {
+        //Left Shift Operator
+        System.out.println(10<<2);//10*2^2=10*4=40
+        System.out.println(20<<1);
+        System.out.println(30<<2);
+        //Right Shift Operator
+        System.out.println(10>>2);//10/2^2=10/4=2
+        System.out.println(20>>2);
+        System.out.println(40>>2);
+    }
+}
