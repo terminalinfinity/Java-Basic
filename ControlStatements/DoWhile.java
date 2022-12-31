85dc4f5be6c02bf854a1bb5c6d6568f83af42279
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2022-12-31 04:43:08 -0800

    Add files via upload

---
+++ ControlStatements/DoWhile.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int count=1;
+        do {
+            System.out.println(count);
+            count++;
+        }
+        while (count<=10);
+
+
+    }
+}
